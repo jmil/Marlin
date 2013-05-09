94f093b8efdc9bb9e2740f197999557246d28390
Jordan Miller <peptides@benchscience.org>
2013-05-09 17:04:21 -0400

    Redefined Pins.h to follow JohnnyR's MotorExt pin diagram and details. Ready to try with ShopBot

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -1118,30 +1118,32 @@
 #error Oops!  Make sure you have 'Arduino Mega 2560' selected from the 'Tools -> Boards' menu.
 #endif
 
-#define X_STEP_PIN 37
-#define X_DIR_PIN 48
+// #define X_STEP_PIN 37
+// #define X_DIR_PIN 48
+#define X_STEP_PIN 32
+#define X_DIR_PIN 44
 #define X_MIN_PIN 12
 #define X_MAX_PIN 19
-#define X_ENABLE_PIN 29
+// #define X_ENABLE_PIN 29
+// USE ShopBot Motor Ext pins with Enable gets X_ENABLE_PIN
+#define X_ENABLE_PIN 22
 #define X_MS1_PIN 40
 #define X_MS2_PIN 41
 
 #define Y_MIN_PIN 11
 #define Y_MAX_PIN 18
-// Switch Y and E1 since Y is broken
-// #define Y_STEP_PIN 33
-// #define Y_DIR_PIN 42
-// #define Y_ENABLE_PIN 25
-// #define Y_MS1_PIN 63
-// #define Y_MS2_PIN 64
-#define Y_STEP_PIN 36
-#define Y_DIR_PIN 49
+// #define Y_STEP_PIN 36
+// #define Y_DIR_PIN 49
+#define Y_STEP_PIN 31
+#define Y_DIR_PIN 45
 #define Y_ENABLE_PIN 28
 #define Y_MS1_PIN 69
 #define Y_MS2_PIN 39
 
-#define Z_STEP_PIN 35
-#define Z_DIR_PIN 47
+// #define Z_STEP_PIN 35
+// #define Z_DIR_PIN 47
+#define Z_STEP_PIN 30
+#define Z_DIR_PIN 46
 #define Z_MIN_PIN 10
 #define Z_MAX_PIN 15
 #define Z_ENABLE_PIN 27
